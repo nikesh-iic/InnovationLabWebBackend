b8795a9e902ff890c59f8ac30299ed9a46c978af
sudankdk <sudankhadka345@gmail.com>
2025-05-01 15:02:31 +0545

    feat: testimonials API

---
+++ InnovationLabBackend.Api/Interfaces/ITestimonials.cs
@@ -0,0 +1,13 @@
+﻿using InnovationLabBackend.Api.DTO.Testimonials;
+using InnovationLabBackend.Api.Models;
+
+namespace InnovationLabBackend.Api.Interfaces
+{
+    public interface ITestimonials
+    {
+        Task<List<Testimonial>> GetTestimonialsAsync();
+        Task<Testimonial> CreateTestimonialAsync(Testimonial testimonial);
+        Task<Testimonial?> UpdateTestimonialAsync(string id,UpdateTestimonialsDTO testimonial);
+        Task<bool> DeleteTestimonialAsync(string id);
+    }
+}
---
+++ InnovationLabBackend.Api/Models/Testimonial.cs
@@ -0,0 +1,14 @@
+﻿namespace InnovationLabBackend.Api.Models
+{
+    public class Testimonial
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string? Designation { get; set; } 
+        public string? Organization { get; set; } 
+        public string? ImageUrl { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
